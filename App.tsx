d9293a975731959bb88f4d4190d0c1c12b9b44ab
Altin Xhaka <altinxhaka1@gmail.com>
2024-09-16 12:10:32 +0200

    type

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-     <Text>aaa</Text>
+     <Text>aaasdasdasd</Text>
       <StatusBar style="auto" />
     </View>
   );
